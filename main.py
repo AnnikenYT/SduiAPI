00a5fa3cc1faa4e85dfa92adc92b50ad9e4dce32
anantha1.coolkopf@outlook.com <anantha1.coolkopf@outlook.com>
2021-07-05 15:50:37 +0200

    Globalisatzion

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 import secrets
 import json
 
+### TABLES: AnnikenYT:305870 BigBoy32:305516
 
-def get_data():
-    url = 'https://api.sdui.app/v1/users/305870/timetable'
+def get_data(target_table):
+    url = f'https://api.sdui.app/v1/users/{target_table}/timetable'
     headers = {
         "authorization": secrets.token,
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -12,5 +13,4 @@ def get_data():
     r = requests.get(url, headers=headers)
     json.dump(r.json(), open("data.json", "w"))
 
-
-
+get_data()
