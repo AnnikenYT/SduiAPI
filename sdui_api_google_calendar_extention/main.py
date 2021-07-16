2ccbd7874c33adfa1069d9121c49826a93dbbe53
AnnikenYT <61291253+AnnikenYT@users.noreply.github.com>
2021-07-16 16:22:14 +0200

    - Added google calendar - added RoomChange as change type to packet - made packet pip-friendly

--- sdui_api_google_calendar_extention/main.py
+++ sdui_api_google_calendar_extention/main.py
@@ -8,27 +8,45 @@
 #                                                            |_|   |_|
 #                           BY vob#1634 and Anniken#0001
 
-### future import, do not touch! ###
-from __future__ import print_function
 
+### Imports, do not touch! ###
+from __future__ import print_function
+from secrets import token
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import os.path
+from datetime import datetime, timedelta
+import secrets
+from time import timezone
 from sduiapi.packet import Wrapper
+
+print(
+    """
+    #   _____     _       _  ___  ______ _____   _    _
+    #  /  ___|   | |     (_)/ _ \ | ___ \_   _| | |  | |
+    #  \ `--.  __| |_   _ _/ /_\ \| |_/ / | |   | |  | |_ __ __ _ _ __  _ __   ___ _ __
+    #   `--. \/ _` | | | | |  _  ||  __/  | |   | |/\| | '__/ _` | '_ \| '_ \ / _ \ '__|
+    #  /\__/ / (_| | |_| | | | | || |    _| |_  \  /\  / | | (_| | |_) | |_) |  __/ |
+    #  \____/ \__,_|\__,_|_\_| |_/\_|    \___/   \/  \/|_|  \__,_| .__/| .__/ \___|_|
+    #                                                            | |   | |
+    #                                                            |_|   |_|
+    #                           BY vob#1634 and Anniken#0001
+    """)
 
 ### Variables ###
 
-CALENDAR_ID = "khopmssvb4f6qbi0rvp7bohr68@group.calendar.google.com"
+CALENDAR_ID = "primary"
+TIME_DELTA = -7
 
 ### Code - Do not touch unless you know what you are doing ###
 
-import datetime
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from sduiapi.packet import Wrapper
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+Wrapper = Wrapper(TOKEN=token, TABLE_ID=305870)
 
 
 def main():
@@ -55,21 +73,55 @@ def main():
 
     service = build('calendar', 'v3', credentials=creds)
 
-        # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
+    
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+                                          maxResults=20, singleEvents=True,
+                                          orderBy='startTime').execute()
+    eventsToday = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    data = Wrapper.get_lessons_for_day(TIME_DELTA)
 
-    ## add today's events ##
+    for i in data:
+        if "oftype" in i:
+            if i["oftype"] == "SUB":
+                name = i["subject"]
+                desc = "New Teacher: " + i["teacher"]
+                color = 4
+            elif i["oftype"] == "CANCLED":
+                name = "Free."
+                color = 8
+            elif i["oftype"] == "ROOM_CHANGE":
+                name = i["subject"]
+                desc = "Room was changed."
+                color = 5
+        else:
+            desc = ""
+            color = 1
+            name = i["subject"]
+        event = create_event(i["begin"], i["end"], name, desc, service, colorId=color)
+        print('Event created: %s' % (event.get('htmlLink')))
+
+
+def create_event(time_start, time_end, name, desc, service_handler, colorId):
+    time_with_delta = datetime.now() - timedelta(TIME_DELTA)
+
+    formdate = '-'.join(str(time_with_delta.strftime("%Y-%D-%M")
+                            ).replace("/", "-").split("-")[0:3])
+
+    GMT_OFF = '+02:00'
+
+    EVENT = {
+        'summary': name,
+        'colorId': colorId,
+        "description": desc,
+        'start': {'dateTime': formdate + "T" + str(timedelta(seconds=time_start+60*60)) + GMT_OFF},
+        'end': {'dateTime': formdate + "T" + str(timedelta(seconds=time_end+60*60)) + GMT_OFF},
+    }
+
+    event = service_handler.events().insert(
+        calendarId=CALENDAR_ID, body=EVENT).execute()
+    return event
 
 
 if __name__ == '__main__':
